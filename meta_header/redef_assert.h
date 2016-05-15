668e6ea585a0bf3e0ff054ee7698bfa474aa245a
724789975 <724789975@qq.com>
2016-05-15 14:34:34 +0800

    忘记提交

---
+++ meta_header/redef_assert.h
@@ -0,0 +1,12 @@
+#ifndef __REDEF_ASSERT_H__
+#define  __REDEF_ASSERT_H__
+
+#include <assert.h>
+
+#ifdef NDEBUG
+#define Assert(expression) (void)(!!(expression))
+#else
+#define Assert assert
+#endif // NDEBUG
+
+#endif
