32a504e60c45901f3239c8a0f40679232780583d
724789975 <724789975@qq.com>
2017-12-31 16:56:59 +0800

    protobuf注册
    Signed-off-by: 724789975 <724789975@qq.com>

--- TestProto/main.cpp
+++ TestProto/main.cpp
@@ -12,7 +12,8 @@ class AAA
 public:
 	AAA(){}
 
-	void Test(BBB a, test t1){}
+	void Test(BBB* a, const test& t1){}
+
 protected:
 private:
 	int a;
@@ -23,8 +24,7 @@ int main(int argc, char **argv)
 {
 	AAA ta;
 	ProtoBufDispatch::ProtoDispatcher<BBB> aaa(std::string("asdf"));
-	ProtoBufDispatch::ProtoDispatcher<ProtoBufDispatch::NullType> a1aa("asd1f");
-	//aaa.FuncReg<test>(std::bind(&AAA::Test, std::placeholders::_1, std::placeholders::_2));
+	aaa.FuncReg<test>(std::bind(&(AAA::Test), &ta, std::placeholders::_1, std::placeholders::_2));
 
 	test ttt;
 	ttt.set_id(123);
