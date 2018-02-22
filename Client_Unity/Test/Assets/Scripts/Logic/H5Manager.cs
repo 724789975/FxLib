9ea2548609f36d0dff32eb7cc0f9024726ac3d84
724789975 <724789975@qq.com>
2018-02-22 20:02:20 +0800

    启动h5页面
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ Client_Unity/Test/Assets/Scripts/Logic/H5Manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H5Manager : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () { }
+	
+	// Update is called once per frame
+	void Update () { }
+
+	void Init(ushort wPlayerPort, ushort wServerPort, ushort wSlaveServerPort)
+	{
+		m_wPlayerPort = wPlayerPort;
+		m_wServerPort = wServerPort;
+		m_wSlaveServerPort = wSlaveServerPort;
+	}
+
+	ushort m_wPlayerPort;
+	ushort m_wServerPort;
+	ushort m_wSlaveServerPort;
+}
