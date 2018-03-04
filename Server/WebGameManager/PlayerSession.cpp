9ddd6700489eb59b5e9d4bc37f492ca7f623b99a
724789975 <724789975@qq.com>
2018-03-04 20:14:01 +0800

    fix
    Signed-off-by: 724789975 <724789975@qq.com>

--- Server/WebGameManager/PlayerSession.cpp
+++ Server/WebGameManager/PlayerSession.cpp
@@ -42,6 +42,7 @@ bool StartProccess(unsigned long long qwPlayerPoint)
 	{
 		LogExe(LogLv_Error, "%s %s %s %s %s %s %s",
 			arg[0], arg[1], arg[2], arg[3], arg[4], arg[5], arg[6]);
+		return false;
 	}
 
 	if (pid == 0)
