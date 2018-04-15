12d0f7c517008a76ed8890757d814f1a226e26e0
724789975 <724789975@qq.com>
2018-04-16 01:36:57 +0800

    redis 漏传
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ Server/RedisCon/redisquery.cpp
@@ -0,0 +1,55 @@
+﻿#include "redisquery.h"
+#include "redisreader.h"
+#include "redismodule.h"
+
+FxRedisQuery::FxRedisQuery()
+{
+}
+
+FxRedisQuery::~FxRedisQuery()
+{
+}
+
+bool FxRedisQuery::ConnectRedis(const std::string& szHost, unsigned int dwPort, std::string szPassword)
+{
+	if(!m_oMySqlConn.Connect(szHost, dwPort, szPassword))                                                                              
+	{
+		LogExe(LogLv_Error, "Connect to redis %s:%d error", szHost.c_str(), dwPort);
+		return false;
+	}
+
+	return true;
+}
+
+void FxRedisQuery::Query(IRedisQuery *poQuery)
+{
+	poQuery->OnQuery(this);
+	poQuery->OnResult();
+	poQuery->Release();
+}
+
+INT32 FxRedisQuery::Query(const char* pszCMD)
+{
+	return m_oMySqlConn.Query(pszCMD);
+}
+
+INT32 FxRedisQuery::Query(const char* pszCMD, IRedisDataReader **ppReader)
+{
+    if (NULL != *ppReader)
+    {
+        return -1;
+    }
+
+    FxRedisReader* poReader = FxRedisModule::Instance()->FetchReader();
+    if (NULL == poReader)
+    {
+        return -1;
+    }
+    *ppReader = poReader;
+
+	INT32 nRetCode = m_oMySqlConn.Query( pszCMD, *poReader);
+
+	return nRetCode;
+}
+
+
---
+++ Server/RedisCon/redisquery.h
@@ -0,0 +1,27 @@
+#ifndef __REDIS_QUERY_H__
+#define __REDIS_QUERY_H__
+
+#include <list>
+#include <time.h>
+#include <fxmeta.h>
+#include "redisconnection.h"
+#include "lock.h"
+
+class FxRedisQuery : public IRedisConnection
+{
+public:
+	FxRedisQuery();
+	virtual ~FxRedisQuery();
+
+	virtual INT32		Query(const char* pszSQL);
+	virtual INT32		Query(const char* pszSQL, IRedisDataReader **ppReader);
+
+	bool ConnectRedis(const std::string& szHost, unsigned int dwPort, std::string szPassword);
+	void		        Query(IRedisQuery* poQuery);
+	
+private:
+	FxRedisConnection	m_oMySqlConn;		
+};
+
+#endif	// __REDIS_QUERY_H__
+
