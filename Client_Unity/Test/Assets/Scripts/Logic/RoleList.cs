068e7b74926a09b17f101d62711b0ac5162dd99a
724789975 <724789975@qq.com>
2018-05-02 01:24:19 +0800

    漏传
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ Client_Unity/Test/Assets/Scripts/Logic/RoleList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleList : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+
+	public void SetPlayerIds(Google.Protobuf.Collections.RepeatedField<ulong> qwPlayerId)
+	{
+		for (int i = 0; i < qwPlayerId.Count; ++i)
+		{
+			GameObject obj = Instantiate(m_pPlayerObj, m_pContentTransform);
+			obj.GetComponent<TeamPlayer>().SetPlayerId(qwPlayerId[i]);
+		}
+	}
+
+	public GameObject m_pPlayerObj;
+	public Transform m_pContentTransform;
+}
---
+++ Client_Unity/Test/Assets/Scripts/Logic/TeamPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void SetPlayerId(ulong qwPlayerId)
+	{
+		m_qwPlayerId = qwPlayerId;
+
+		m_pButton.onClick.AddListener(delegate ()
+		{
+			InvitePlayer();
+		});
+
+		m_txtPlayerId.text = "invite : " + m_qwPlayerId.ToString();
+	}
+
+	public void InvitePlayer()
+	{
+		GameProto.PlayerRequestLoginInviteTeam oTeam = new GameProto.PlayerRequestLoginInviteTeam();
+		oTeam.QwPlayerId = m_qwPlayerId;
+		byte[] pData = new byte[1024];
+		FxNet.NetStream pStream = new FxNet.NetStream(FxNet.NetStream.ENetStreamType.ENetStreamType_Write, pData, 1024);
+		pStream.WriteString("GameProto.PlayerRequestLoginInviteTeam");
+		byte[] pProto = new byte[oTeam.CalculateSize()];
+		Google.Protobuf.CodedOutputStream oStream = new Google.Protobuf.CodedOutputStream(pProto);
+		oTeam.WriteTo(oStream);
+		pStream.WriteData(pProto, (uint)pProto.Length);
+
+		H5Manager.Instance().GetLoginSession().Send(pData, 1024 - pStream.GetLeftLen());
+
+		//m_pSession.Send(pData, 1024 - pStream.GetLeftLen());
+	}
+
+	public ulong m_qwPlayerId;
+	public UnityEngine.UI.Text m_txtPlayerId;
+	public UnityEngine.UI.Button m_pButton;
+}
