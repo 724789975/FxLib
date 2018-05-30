61bcf2cc91a3819c6f036137005314d0f64a8058
724789975 <724789975@qq.com>
2018-05-30 21:18:00 +0800

    SceneInit
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ Client_Unity/Test/Assets/Scripts/Logic/SceneInit/ChoseServerInit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoseServerInit : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameInstance.Instance().GetServerInfo();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
+
