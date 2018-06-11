98c70c9d06ccebca44a2b58daa701b359b58d088
724789975 <724789975@qq.com>
2018-06-12 00:52:32 +0800

    游戏场景
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ Client_Unity/Test/Assets/Scripts/Logic/game/GameLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : SingletonObject<GameLogic>
+{
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	public UnityEngine.UI.Image[] m_arrBlocks = new UnityEngine.UI.Image[22*12];
+	public UnityEngine.UI.Image[] m_arrNextBlocks = new UnityEngine.UI.Image[4*4];
+	public UnityEngine.UI.Image[] m_arrCurrBlocks = new UnityEngine.UI.Image[4*4];
+}
+
