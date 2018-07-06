385282850d572fb4fd37fe94e06fd54bce313147
724789975 <724789975@qq.com>
2018-07-06 11:44:12 +0800

    大厅界面修改
    Signed-off-by: 724789975 <724789975@qq.com>

--- Client_Unity/Test/Assets/Scripts/UI/LobbyUI.cs
+++ Client_Unity/Test/Assets/Scripts/UI/LobbyUI.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class LobbyUI : SingletonObject<LobbyUI>
 {
+	void Awake()
+	{
+		CreateInstance(this);
+	}
 	// Use this for initialization
 	void Start ()
 	{
-		CreateInstance(this);
-
 		m_buttonMakeTeam.onClick.AddListener(delegate () { LoginControler.Instance().MakeTeam(); });
 		m_buttonTeamStart.onClick.AddListener(delegate () { LoginControler.Instance().TeamStart(); });
 		m_buttonOnlinePlayers.onClick.AddListener(delegate () { LoginControler.Instance().OnlinePlayers(); });
 		m_buttonLeave.onClick.AddListener(delegate () { LoginControler.Instance().LeaveTeam(); });
+
+		m_textName.text = PlayerData.Instance().proName;
+		StartCoroutine(H5Helper.SendGet(PlayerData.Instance().proHeadImage, delegate (Texture2D tex)
+			{
+				m_imgHead.texture = tex;
+			})
+		);
 	}
 	
 	// Update is called once per frame
@@ -24,4 +33,7 @@ public class LobbyUI : SingletonObject<LobbyUI>
 	public UnityEngine.UI.Button m_buttonTeamStart;
 	public UnityEngine.UI.Button m_buttonOnlinePlayers;
 	public UnityEngine.UI.Button m_buttonLeave;
+	public UnityEngine.UI.RawImage m_imgHead;
+	public UnityEngine.UI.Image m_imgFrame;
+	public UnityEngine.UI.Text m_textName;
 }
