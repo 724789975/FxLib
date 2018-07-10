56bb05b08c221b6d177f1aa4387c9c822bef312c
724789975 <724789975@qq.com>
2018-07-10 18:17:14 +0800

    fix
    Signed-off-by: 724789975 <724789975@qq.com>

---
+++ Client_Unity/Test/Assets/Scripts/Logic/LobbyTeam/LobbyTeamPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyTeamPlayer : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void Init(GameProto.RoleData oData)
+	{
+		m_qwPlayerId = oData.QwPlayerId;
+		m_buttonKick.gameObject.SetActive(true);
+		m_textName.text = oData.SzNickName;
+		m_buttonInvite.gameObject.SetActive(false);
+		StartCoroutine(H5Helper.SendGet(oData.SzAvatar, delegate (Texture2D tex)
+			{
+				m_imgHead.texture = tex;
+				m_imgHead.gameObject.SetActive(true);
+			})
+		);
+	}
+
+	[UnityEngine.SerializeField]
+	uint m_dwSlotId;
+	[UnityEngine.SerializeField]
+	System.UInt64 m_qwPlayerId;
+
+	[UnityEngine.SerializeField]
+	UnityEngine.UI.RawImage m_imgHead = null;
+	[UnityEngine.SerializeField]
+	UnityEngine.UI.Button m_buttonKick = null;
+	[UnityEngine.SerializeField]
+	UnityEngine.UI.Text m_textName = null;
+	[UnityEngine.SerializeField]
+	UnityEngine.UI.Button m_buttonInvite = null;
+}
+
---
+++ Client_Unity/Test/Assets/Scripts/Logic/LobbyTeam/TeamPlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamPlayerManager : SingletonObject<TeamPlayerManager>
+{
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	public void SyncTeamInfo(GameProto.LoginNotifyPlayerTeamInfo oInfo)
+	{
+		for (int i = 0; i < oInfo.TeamRoleData.Count; i++)
+		{
+			LobbyTeamPlayer pPlayer = GetPlayerBySlot(oInfo.TeamRoleData[i].DwSlotId);
+			pPlayer.Init(oInfo.TeamRoleData[i].RoleData);
+		}
+	}
+
+	public LobbyTeamPlayer GetPlayerBySlot(uint dwSlotId)
+	{
+		if (dwSlotId > m_arrRedPlayers.Length)
+		{
+			return m_arrBluePlayers[dwSlotId - m_arrRedPlayers.Length];
+		}
+		return m_arrBluePlayers[dwSlotId];
+	}
+
+	[SerializeField]
+	LobbyTeamPlayer[] m_arrRedPlayers = null;
+	LobbyTeamPlayer[] m_arrBluePlayers = null;
+}
+
