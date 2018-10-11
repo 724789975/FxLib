d1c9aa731665ac7d622a50aceb5127ba46ca233d
724789975 <724789975@qq.com>
2018-10-11 17:41:09 +0800

    不能依赖其他文件

--- Server/common/exception_dump.cpp
+++ Server/common/exception_dump.cpp
@@ -1,9 +1,8 @@
 #include "exception_dump.h"
 #include "segvcatch.h"
-#include "fxtimer.h"
-#include "fxmeta.h"
 
 #include <stdexcept>
+#include <time.h>
 
 #ifdef WIN32
 #include <windows.h>
@@ -12,13 +11,68 @@
 
 namespace ExceptionDump
 {
+	char* GetExeName()
+	{
+		static char strExePath[256] =
+		{ 0 };
+		static bool bInited = false;
+
+		if (!bInited)
+		{
+#ifdef WIN32
+			//GetCurrentDirectory(256, strWorkPath);
+
+			GetModuleFileName(NULL, strExePath, 256);
+			for (int i = strlen(strExePath); i >= 0; i--)
+			{
+				if (strExePath[i] == '.')
+				{
+					strExePath[i] = 0;
+				}
+				if (strExePath[i] == '\\')
+				{
+					memcpy(strExePath, &(strExePath[i + 1]), 256 - (i + 1) - 1);
+					//strExePath[i] = '\0';
+					break;
+				}
+			}
+#else
+			char strSysfile[256] =
+			{ 0 };
+			sprintf(strSysfile, "/proc/%d/exe", getpid());
+
+			int nRet = readlink(strSysfile, strExePath, 256);
+			if ((nRet > 0) & (nRet < 256))
+			{
+				bInited = true;
+
+				strExePath[nRet] = 0;
+
+				for (int i = nRet; i >= 0; --i)
+				{
+					if (strExePath[i] == '/')
+					{
+						memcpy(strExePath, &(strExePath[i + 1]), 256 - (i + 1) - 1);
+						//strExePath[i] = '\0';
+						break;
+					}
+					//strExePath[i] = '\0';
+				}
+			}
+#endif
+		}
+
+		return strExePath;
+	}
+
 #ifndef WIN32
 	void MakeDump()
 	{
+		time_t tm = time(NULL);
 		static unsigned int g_dwLastExceptionTime = 0;
-		if (GetTimeHandler()->GetSecond() - g_dwLastExceptionTime > 1800)
+		if (tm - g_dwLastExceptionTime > 1800)
 		{
-			g_dwLastExceptionTime = GetTimeHandler()->GetSecond();
+			g_dwLastExceptionTime = tm;
 
 			char szCmd[256] = { 0 };
 			snprintf(szCmd, sizeof(szCmd) - 1, "gcore -o core_%s_%d %u", GetExeName(), g_dwLastExceptionTime, getpid());
