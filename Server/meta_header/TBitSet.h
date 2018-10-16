10819e6d37c63ac55333e4c6129e1de98e7f83b4
724789975 <724789975@qq.com>
2018-10-16 10:50:48 +0800

    换名字
    Signed-off-by: 724789975 <724789975@qq.com>

--- Server/meta_header/TBitSet.h
+++ Server/meta_header/TBitSet.h
@@ -1,23 +1,23 @@
-#ifndef __TByteSet_h__
-#define __TByteSet_h__
+#ifndef __TBitSet_h__
+#define __TBitSet_h__
 
 #include <string.h>
 
 //这个类型的对象根本不会被创建成功
-#define EnumByteSet(EnumName) \
+#define EnumBitSet(EnumName) \
 template<EnumName>\
-class T##EnumName##ByteSet\
+class T##EnumName##BitSet\
 {\
 private:\
-	T##EnumName##ByteSet();\
+	T##EnumName##BitSet();\
 };\
 template<>\
-class T##EnumName##ByteSet<EnumName##Count>\
+class T##EnumName##BitSet<EnumName##Count>\
 {\
 public:\
-	T##EnumName##ByteSet(){memset(m_pData, 0, sizeof(m_pData));}\
-	virtual ~T##EnumName##ByteSet(){}\
-	bool SetByte(EnumName e, bool bValue)\
+	T##EnumName##BitSet(){memset(m_pData, 0, sizeof(m_pData));}\
+	virtual ~T##EnumName##BitSet(){}\
+	bool SetBit(EnumName e, bool bValue)\
 	{\
 		if (e >= EnumName##Count)\
 		{\
@@ -33,7 +33,7 @@ public:\
 		}\
 		return true;\
 	}\
-	bool GetByte(EnumName e)\
+	bool GetBit(EnumName e)\
 	{\
 		if (e >= EnumName##Count)\
 		{\
@@ -48,16 +48,16 @@ public:\
 private:\
 	char m_pData[EnumName##Count / 8 + 1];\
 };\
-typedef T##EnumName##ByteSet<EnumName##Count> EnumName##ByteSet;
+typedef T##EnumName##BitSet<EnumName##Count> EnumName##BitSet;
 
-#define BeginEnumByteSet(EnumName) \
+#define BeginEnumBitSet(EnumName) \
 enum EnumName
 
-#define EndEnumByteSet(EnumName) \
+#define EndEnumBitSet(EnumName) \
 EnumName##Count,
 
-#define EndEnumByteSetWithCount(EnumName, NUM) \
+#define EndEnumBitSetWithCount(EnumName, NUM) \
 EnumName##Count = NUM,
 
 
-#endif // !__TByteSet_h__
+#endif // !__TBitSet_h__
