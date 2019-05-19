fcc307fbb1e1e5fc5521f8016e34dc2b4dea7248
724789975 <724789975@qq.com>
2019-05-19 21:21:07 +0800

    判断类型是否相同
    Signed-off-by: 724789975 <724789975@qq.com>

--- Server/property/integral_constant.h
+++ Server/property/integral_constant.h
@@ -12,4 +12,8 @@ struct integral_constant
 typedef integral_constant<bool, true>  true_type;
 typedef integral_constant<bool, false> false_type;
 
+template<typename, typename> struct is_same : public false_type { };
+template<typename _Tp> struct is_same<_Tp, _Tp>: public true_type { };
+
+
 #endif
